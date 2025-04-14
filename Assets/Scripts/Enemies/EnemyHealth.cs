8c1869fe957866f1cde2a1117ad44fbd3ac40887
NguyenNhatMinh <mattroitoasang2003@gmail.com>
2025-04-15 01:38:21 +0700

    feat: add functions and levels

---
+++ Assets/Scripts/Enemies/EnemyHealth.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] private int staringHealth = 3;
+
+    [SerializeField] private GameObject deathVFXPrefab;
+
+    [SerializeField] private float knockBackThrust = 15f;
+
+    private int currentHealth;
+
+    private Knockback knockback;
+
+    private Flash flash;
+
+    private void Awake()
+    {
+        flash = GetComponent<Flash>();
+        knockback = GetComponent<Knockback>();
+    }
+    private void Start()
+    {
+        currentHealth = staringHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        knockback.GetKnockedBack(PlayerController.Instance.transform, knockBackThrust);
+        StartCoroutine(flash.FlashRoutine());
+        StartCoroutine(CheckDetectDeathRoutine());
+    }
+
+    private IEnumerator CheckDetectDeathRoutine()
+    {
+        yield return new WaitForSeconds(flash.GetRestoreMatTime());
+        DetectDeath();
+    }
+
+    public void DetectDeath()
+    {
+        if (currentHealth <= 0)
+        {
+            Instantiate(deathVFXPrefab, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
